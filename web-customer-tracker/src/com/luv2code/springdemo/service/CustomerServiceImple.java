471e59edd0eedc11b0c37f1e48c6713a54b11e25
Denis Kisina <kisinad@gmail.com>
2020-07-30 17:18:13 -0500

    Added the deletion of customer from db

--- web-customer-tracker/src/com/luv2code/springdemo/service/CustomerServiceImple.java
+++ web-customer-tracker/src/com/luv2code/springdemo/service/CustomerServiceImple.java
@@ -37,4 +37,11 @@ public class CustomerServiceImple implements CustomerService {
 		return customerDAO.getCustomer(theId);
 	}
 
+	@Override
+	@Transactional
+	public void deleteCustomer(int theId) {
+		customerDAO.deleteCustomer(theId);
+
+	}
+
 }
