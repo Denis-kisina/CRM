e60106dc91abf899fc5662142f671c03a86bbff4
Denis Kisina <kisinad@gmail.com>
2020-07-29 21:33:33 -0500

    DB connection test success!

--- web-customer-tracker/src/com/luv2code/testdb/TestDbServlet.java
+++ web-customer-tracker/src/com/luv2code/testdb/TestDbServlet.java
@@ -1,6 +1,9 @@
 package com.luv2code.testdb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -28,6 +31,25 @@ public class TestDbServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		String user = "springstudent";
+		String pass = "springstudent";
+
+		String JdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+		String driver = "com.mysql.cj.jdbc.Driver";
+
+		try {
+			PrintWriter out = response.getWriter();
+			out.println("Connecting to DB " + JdbcUrl);
+
+			Class.forName(driver);
+			Connection myConnection = DriverManager.getConnection(JdbcUrl, user, pass);
+			out.println("Success!!");
+			myConnection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
 
 	}
 
